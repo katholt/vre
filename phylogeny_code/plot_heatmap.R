6ecf92cc2c86c54dd7e4d1e8258c96e33a617c02
Paul McAdam <prmcadam@gmail.com>
2015-02-12 15:36:20 +1100

    add code for plotting snp/tree heatmap

---
+++ phylogeny_code/plot_heatmap.R
@@ -0,0 +1,4 @@
+tree.file <- read.tree('/vlsci/VR0082/shared/data/enterococcus/faecium/RedDogv51_How2013Alf2015_CP006620/CP006620_CP006620_alleles_var_cons0.95.tree')
+ingroup.tree<-drop.tip(tree.file,c('aus0013_AC026VACXX_CAGATC_L005','aus0028_AC026VACXX_ATCACG_L006','aus0103_AC026VACXX_GATCAG_L008'))
+recombination.density <- getRecombHeatmap(snp.table,ref="Ref",w=10000)
+plotTree(tree=ingroup.tree, heatmapData=recombination.density,heatmap.colours=colorRampPalette(c("white","orange","red"),space="rgb")(100), colLabelCex=0.0001)
